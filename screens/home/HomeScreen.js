571cc4b972ffe9d6b663482170b247c7a67187a0
beibeixx <shelly0424x@gmail.com>
2024-11-10 15:44:04 -0800

    display all the read-only playground data on home page.

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,10 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import dataService, { fetchData } from '../../services/dataService'
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation}) {
+  const [data, setData] = useState([])
+  const [filteredData, setFilteredData] = useState([])
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    try {
+      const items = fetchData();
+      setData(items);
+      setFilteredData(items);
+    } catch(err) {
+      console.log("Fetch data error",err)
+    }
+  }
+
+  const renderItem = ({item}) => {
+    return(
+    <TouchableOpacity>
+      <View>
+        <Image source={{ uri: item.images[0]}} style={{width: 100, height: 100}}/>
+        <Text>{item.name}</Text>
+      </View>
+    </TouchableOpacity>
+    )
+  }
+
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <FlatList
+        data={filteredData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      /> 
     </View>
   )
 }

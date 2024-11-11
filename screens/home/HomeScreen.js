cd9dd8f674b47b4d2f733f42aeeb063f2f94e1f7
beibeixx <shelly0424x@gmail.com>
2024-11-10 22:33:21 -0800

    Add the search function on the home page.

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,10 +1,21 @@
-import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image, Pressable} from 'react-native'
-import React, { useState, useEffect } from 'react'
-import dataService, { fetchData } from '../../services/dataService'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import dataService, { fetchData } from "../../services/dataService";
+import SearchBar from "../../components/home/SearchBar";
 
-export default function HomeScreen({ navigation}) {
-  const [data, setData] = useState([])
-  const [filteredData, setFilteredData] = useState([])
+export default function HomeScreen({ navigation }) {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     loadData();
@@ -15,31 +26,49 @@ export default function HomeScreen({ navigation}) {
       const items = fetchData();
       setData(items);
       setFilteredData(items);
-    } catch(err) {
-      console.log("Fetch data error",err)
+    } catch (err) {
+      console.log("Fetch data error", err);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <Pressable>
+        <View>
+          <Image
+            source={{ uri: item.images[0] }}
+            style={{ width: 100, height: 100 }}
+          />
+          <Text>{item.name}</Text>
+        </View>
+      </Pressable>
+    );
+  };
+
+  if (loading) {
+    return <ActivityIndicator size="large" />;
   }
 
-  const renderItem = ({item}) => {
-    return(
-    <Pressable>
-      <View>
-        <Image source={{ uri: item.images[0]}} style={{width: 100, height: 100}}/>
-        <Text>{item.name}</Text>
-      </View>
-    </Pressable>
-    )
-  }
+
+  const handleSearch = (search) => {
+    const result = data.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredData(result);
+  };
 
   return (
     <View>
+      <SearchBar onSearch={handleSearch} />
       <FlatList
         data={filteredData}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
-      /> 
+        keyExtractor={(item) => item.id}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

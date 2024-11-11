b9e07f48e7f87d4f0fca3d3caaeece91160baa61
ElAleph2049 <yiheru@gmail.com>
2024-11-10 17:03:20 -0800

    Add PressableButton component
    - Created PressableButton component to handle press events.
    - Applied custom styles and ripple effect on press.
    - Accepts children, pressHandler, and componentStyle as props.

---
+++ components/common/PressableButton.js
@@ -0,0 +1,19 @@
+import { Pressable } from 'react-native'
+import React from 'react'
+import commonStyles from '.../utils/style'
+import commonColors from '.../utils/color'
+
+export default function PressableButton({children, pressHandler, componentStyle}) {
+  return (
+    <Pressable 
+      onPress={pressHandler}
+      style={({ pressed }) => [
+        componentStyle,
+        pressed && commonStyles.pressed,
+      ]}
+        android_ripple={{color: commonColors.itemBackgroundSelected}}
+      >
+      {children}
+    </Pressable>
+  )
+}
--- utils/color.js
+++ utils/color.js
@@ -1,1 +1,7 @@
 // store all the color for styles
+
+const commonColors = {
+  itemBackgroundSelected: '#eacbae',
+};
+
+export default commonColors;

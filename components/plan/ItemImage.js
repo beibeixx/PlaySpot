de6bfb26e7bb9f8b2cbd75d1ee08292a7941033d
ElAleph2049 <yiheru@gmail.com>
2024-11-11 20:38:38 -0800

    feat: Add ItemImage component to display images by ID
    - Implemented ItemImage component to fetch and display images using getItemImageById.

---
+++ components/plan/ItemImage.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Image } from 'react-native'
+import React from 'react'
+import { getItemImageById } from '../../services/dataService'
+
+export default function ItemImage( {id}) {
+  const imageUri = getItemImageById(id);
+  console.log(imageUri);
+  return (
+    <Image
+    source={{ uri: imageUri}}
+    style={{ width: 300, height: 100 }}
+    />
+  )
+}
+
+const styles = StyleSheet.create({})

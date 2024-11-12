aaab64cc69735269451fc45991e0585a3ad4cba2
ElAleph2049 <yiheru@gmail.com>
2024-11-11 20:45:48 -0800

    feat: Add Screen component with SafeAreaView
    - Implemented Screen component to wrap children with SafeAreaView.

---
+++ components/common/Screen.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet} from 'react-native'
+import React from 'react'
+
+export default function Screen( {children}) {
+  return (
+    <SafeAreaView style={styles.screen}>
+      {children}
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  }
+})

8889be5d9dd1e5380253411d560e000cc6764c18
beibeixx <shelly0424x@gmail.com>
2024-11-12 01:42:30 -0800

    update the account screen display.

--- screens/account/AccountScreen.js
+++ screens/account/AccountScreen.js
@@ -2,30 +2,43 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { auth } from "../../firebase/firebaseSetup";
 
-export default function AccountScreen({navigation}) {
-
+export default function AccountScreen({ navigation }) {
   const favoriteHandle = () => {
     navigation.navigate("Favorite List");
+  };
+
+  const handleLogin = () => {
+    navigation.navigate("Login")
   }
 
-
   return (
-    <View>
+    <View sytle={styles.container}>
       {auth.currentUser ? (
         <View>
           <Text>{auth.currentUser.email}</Text>
           <Text>{auth.currentUser.uid}</Text>
-        </View>
-      ) : (
-        <View>
-          {/* FOR TEST FAV ONLY */}
           <Pressable onPress={favoriteHandle}>
             <Text>Favorite List</Text>
           </Pressable>
+        </View>
+      ) : (
+        <View>
+            <Text>Login to get access to all the features!</Text>
+            <Pressable onPress={handleLogin}>
+              <Text>Login</Text>
+            </Pressable>
         </View>
       )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#aaa",
+    borderRadius: 5,
+    margin: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});

d1d59b8432addf1a4af6ec4e7e0a4ce4ec9bc8b9
ElAleph2049 <yiheru@gmail.com>
2024-11-12 10:46:50 -0800

    add modifymemory screen

--- screens/memory/MemoryScreen.js
+++ screens/memory/MemoryScreen.js
@@ -1,7 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect} from 'react'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import PressableButton from '../../components/common/PressableButton'
 
-export default function MemoryScreen() {
+export default function MemoryScreen( {navigation}) {
+
+  useEffect(() => {
+    // Set header options with a Pressable button
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <PressableButton
+            pressHandler={() => navigation.navigate('Modify Memory', {item: null})}
+          >
+            <MaterialIcons name="add-box" size={24} color="white" />
+          </PressableButton>
+        );
+      },
+    });
+  }, [navigation]);
   return (
     <View>
       <Text>MemoryScreen</Text>

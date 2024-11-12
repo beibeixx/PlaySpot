2569f1737ba07de8e82e5b6e23bf6ee2549adfc3
ElAleph2049 <yiheru@gmail.com>
2024-11-11 22:25:18 -0800

    feat: Add PlanDetailsScreen with image gallery and formatted date
    - Implemented PlanDetailsScreen to display plan details.
    - Added useEffect to set the navigation title to the plan name.
    - Fetched playground name and images using dataService functions.
    - Implemented renderImage function to display images in a FlatList.
    - Added formatDate function to format date as yyyy/mm/dd weekday hh:mm.

--- screens/plan/PlanDetailsScreen.js
+++ screens/plan/PlanDetailsScreen.js
@@ -1,10 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native'
+import React, { useEffect } from 'react'
+import { getItemNameById, getImagesById } from '../../services/dataService';
 
-export default function PlanDetailsScreen() {
+export default function PlanDetailsScreen( {navigation, route} ) {
+  const { item } = route.params;
+  const images = getImagesById(item.playgroundId);
+  const playgroundName = getItemNameById(item.playgroundId);
+  
+  useEffect(() => {
+    navigation.setOptions({
+      title: item.planName,
+    })
+  }, [navigation, item]);
+
+  function renderImage({item}) {
+    return <Image
+      source={{ uri: item }}
+      style={{ width: 350, height: 200 }} />
+  }
+
+  function formatDate(date) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'long' };
+    const dateString = date.toLocaleDateString('en-US', options);
+    const [weekday, datePart] = dateString.split(', ');
+    const [month, day, year] = datePart.split('/'); 
+    const timeString = date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    return `${year}/${month}/${day} ${weekday} ${timeString}`;
+};
+
+
   return (
     <View>
-      <Text>PlanDetailsScreen</Text>
+      <Text>{playgroundName}</Text>
+      <Text>{formatDate(item.time.toDate())}</Text>
+      <FlatList
+        data={images} // Assuming item.images is an array of image URLs
+        renderItem={renderImage}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   )
 }

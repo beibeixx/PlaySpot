76612419743a5d38040397728a2f957611930831
ElAleph2049 <yiheru@gmail.com>
2024-11-11 21:28:24 -0800

    Create PlanDetailsScreen

---
+++ screens/plan/PlanDetailsScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PlanDetailsScreen() {
+  return (
+    <View>
+      <Text>PlanDetailsScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})

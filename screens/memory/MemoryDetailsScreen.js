1ae0bbd173f0e33a246d78ab8a4db8e4317fd1bf
ElAleph2049 <yiheru@gmail.com>
2024-11-12 20:44:23 -0800

    feat: Add memory listing and navigation in MemoryScreen
    - Implemented useEffect to fetch and listen for real-time updates from the 'memory' collection.
    - Added renderItem function to display memory details and navigate to Memory Details screen.
    - Used PressableButton to handle navigation and display memory information.
    - Displayed memory name, playground image, and formatted time.

--- screens/memory/MemoryDetailsScreen.js
+++ screens/memory/MemoryDetailsScreen.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function ModifyMemoryScreen() {
+export default function MemoryDetailsScreen() {
   return (
     <View>
-      <Text>ModifyMemoryScreen</Text>
+      <Text>MemoryDetailsScreen</Text>
     </View>
   )
 }

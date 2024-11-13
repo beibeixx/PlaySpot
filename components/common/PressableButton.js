17f98680d58807968484975e1f232d706db0ea22
ElAleph2049 <yiheru@gmail.com>
2024-11-12 20:08:03 -0800

    Added disabled prop and style

--- components/common/PressableButton.js
+++ components/common/PressableButton.js
@@ -3,15 +3,17 @@ import React from 'react'
 import commonStyles from '../../utils/style'
 import commonColors from '../../utils/color'
 
-export default function PressableButton({children, pressHandler, componentStyle}) {
+export default function PressableButton({children, pressHandler, componentStyle, disabled}) {
   return (
     <Pressable 
       onPress={pressHandler}
       style={({ pressed }) => [
         componentStyle,
+        disabled && commonStyles.disabled,
         pressed && commonStyles.pressed,
       ]}
         android_ripple={{color: commonColors.itemBackgroundSelected}}
+      disabled={disabled}
       >
       {children}
     </Pressable>

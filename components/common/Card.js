d4b25f82eebbc480c86a784c1e6bc58a75661474
ElAleph2049 <yiheru@gmail.com>
2024-11-13 11:36:35 -0800

    Add card component for Modal cards

--- components/common/Card.js
+++ components/common/Card.js
@@ -1,1 +1,33 @@
-// Reuseable card 
+// Reuseable card
+import { Modal, StyleSheet, View } from 'react-native'
+import React from 'react'
+
+export default function Card( {children, isVisible, onBack} ) {
+  return (
+    <Modal
+      animationType="slide"
+      visible={isVisible}
+      onRequestClose={onBack}
+      transparent={true}>
+      <View style={styles.card}>
+        {children}
+      </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    width: '80%',
+    maxWidth: 400,
+    justifyContent: 'space-around',
+    elevation: 5,
+  },
+})

25414f26f0c769e60fa8cfe63f623422e8c78d02
ElAleph2049 <yiheru@gmail.com>
2024-11-21 14:57:29 -0800

    Added ImageManager component to take photoes

--- firebase/firebaseSetup.js
+++ firebase/firebaseSetup.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeAuth , getReactNativePersistence} from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,3 +26,4 @@ export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
 // const analytics = getAnalytics(app);
+export const storage = getStorage(app);

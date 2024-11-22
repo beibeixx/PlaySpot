cbb28143a44191bfbf0031cf056bc18f4ddfe809
ElAleph2049 <yiheru@gmail.com>
2024-11-21 16:51:51 -0800

    feat: Implement ImageManager component for handling image selection
    - Added ImageManager component to handle image selection and permissions.
    - Integrated expo-image-picker for camera and gallery access.
    - Added verifyPermission function to request and verify camera permissions.
    - Included MaterialCommunityIcons for image selection UI.
    - Implemented state management for selected image URI.

--- components/memory/AddMemoryPhotoCard.js
+++ components/memory/AddMemoryPhotoCard.js
@@ -2,16 +2,24 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import Card from '../common/Card'
 import PressableButton from '../common/PressableButton';
+import ImageManager from '../common/ImageManager';
 
 export default function AddMemoryPhotoCard( {isVisible, cancelHandler} ) {
-  
+
   function submitHandler() {
     console.log('submit');
     cancelHandler();
-  } 
+  }
+
+  function receiveImageUri(uri) {
+    console.log('Received image uri:', uri);
+    setImageUri(uri);
+}
 
   return (
     <Card isVisible={isVisible} onBack={cancelHandler}>
+      <Text>Memory Photos:</Text>
+      <ImageManager receiveImageUri={receiveImageUri} />
       <View style={styles.buttonContainer}>
         <PressableButton
           pressHandler={cancelHandler}>

3f607dd0d87f6c1c811451fec7c0470a60cafec3
beibeixx <shelly0424x@gmail.com>
2024-11-23 11:14:48 -0800

    Style the plan screen main toggle and container

--- components/plan/PlanList.js
+++ components/plan/PlanList.js
@@ -8,6 +8,8 @@ import { formatDate } from "../../utils/helpers";
 import commonStyles from "../../utils/style";
 import { auth } from "../../firebase/firebaseSetup";
 import { useSelector } from "react-redux";
+import { planListStyles } from "../../styles/components/planList";
+import { cardStyles } from "../../styles/components/card";
 
 export default function PlanList({ timetype, navigation }) {
   const [plans, setPlans] = useState([]);
@@ -49,26 +51,46 @@ export default function PlanList({ timetype, navigation }) {
     return (
       <PressableButton
         pressHandler={() => navigation.navigate("Plan Details", { item })}
-        componentStyle={commonStyles.itemCard}
+        componentStyle={planListStyles.planCard}
       >
         <ItemImage id={item.playgroundId} />
-        <Text style={commonStyles.planName}>{item.planName}</Text>
-        <Text style={commonStyles.timeText}>
-          {formatDate(item.time.toDate())}
-        </Text>
+        <View style={planListStyles.cardContent}>
+          <Text style={planListStyles.planName}>{item.planName}</Text>
+          <Text style={planListStyles.timeText}>
+            {formatDate(item.time.toDate())}
+          </Text>
+        </View>
       </PressableButton>
     );
   }
 
+  // console.log(plans.length)
+  if (plans.length === 0) {
+    return (
+      <View style={planListStyles.emptyContainer}>
+        <Text style={planListStyles.emptyText}>
+          {isAuthenticated 
+            ? `No ${timetype} plans yet`
+            : "Please login to view your plans"}
+        </Text>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <FlatList data={plans} renderItem={renderItem} />
-    </View>
+    // <View style={styles.container}>
+    //   <FlatList data={plans} renderItem={renderItem} />
+    // </View>
+
+        <FlatList
+          data={plans}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          contentContainerStyle={planListStyles.listContainer}
+          showsVerticalScrollIndicator={false}
+        />
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
 });

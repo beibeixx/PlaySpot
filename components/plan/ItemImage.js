1bc660f800c097cd14989d47ca95d63d5cd2c560
ElAleph2049 <yiheru@gmail.com>
2024-11-22 19:39:14 -0800

    feat: Fetch and display the last photo in ItemImage component
    - Updated ItemImage component to fetch and display the last photo from the item.photos array.
    - Used useEffect to fetch the image URL when the component mounts or the item prop changes.
    - Fetched the last photo URL using getDownloadURL from Firebase Storage.
    - Displayed the fetched image URL in the Image component.
    - Fallback to default image URI if item.photos is not available.

--- components/plan/ItemImage.js
+++ components/plan/ItemImage.js
@@ -1,15 +1,41 @@
-import { StyleSheet, Image } from 'react-native'
-import React from 'react'
-import { getItemImageById } from '../../services/dataService'
+import { StyleSheet, Image } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { getItemImageById } from '../../services/dataService';
+import { ref, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../firebase/firebaseSetup';
 
-export default function ItemImage( {id}) {
-  const imageUri = getItemImageById(id);
+export default function ItemImage({ item }) {
+  const [imageUri, setImageUri] = useState('');
+
+  useEffect(() => {
+    async function fetchLastImage() {
+      if (item && item.photos && item.photos.length > 0) {
+        const lastImage = item.photos[item.photos.length - 1];
+        const imageRef = ref(storage, lastImage);
+        try {
+        const httpsImageUri = await getDownloadURL(imageRef);
+        setImageUri(httpsImageUri);
+        } catch (error) {
+          console.error('Error getting image uri:', error);
+        }
+      } else {
+        setImageUri(getItemImageById(item.playgroundId));
+      }
+    }
+    fetchLastImage();
+  }, [item]);
+  
   return (
     <Image
-    source={{ uri: imageUri}}
-    style={{ width: 350, height: 100 }}
+      source={{ uri: imageUri }}
+      style={styles.image}
     />
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    width: 350,
+    height: 100,
+  },
+});
